4431675b5476279cb7f0a949baae732a9e3be357
Daniil <daniil@unknown-rp.net>
2023-03-13 17:16:48 +0100

    Add Follow Up

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ client.on('interactionCreate', async (interaction) => {
 
         // check if the interacting member has a role with one of the allowed group IDs
         const roles = process.env.ACCES_GROUPS.split(',');
-        
+
         const hasAllowedRole = interaction.member.roles.cache.some(r => roles.includes(r.id));
         // Restrict Acces to some group. We don't want indefinite git issues, do we?. Maybe we should even restrict some group from using it even if they have the role.
         if (!hasAllowedRole) {
             return interaction.reply({
                 content: 'You do not have permission to use this command.',
-                ephemeral: true 
+                ephemeral: true
             });
         }
 
@@ -41,86 +41,138 @@ client.on('interactionCreate', async (interaction) => {
             .setCustomId('repo')
             .setLabel("What is the name of the Repo?")
             .setStyle(TextInputStyle.Short);
-
         const titleInput = new TextInputBuilder()
             .setCustomId('title')
             .setLabel("What should be the title of the issue?")
-            .setStyle(TextInputStyle.Short);
+            .setStyle(TextInputStyle.Short)
+            .setRequired(false);
+        const oldIssueNumber = new TextInputBuilder()
+            .setCustomId('oldIssueNumber')
+            .setLabel("Existing Issue Number")
+            .setStyle(TextInputStyle.Short)
+            .setRequired(false);
         const repoActionRow = new ActionRowBuilder().addComponents(repoInput);
+        const oldIssueNumberActionRow = new ActionRowBuilder().addComponents(oldIssueNumber);
         const titleActionRow = new ActionRowBuilder().addComponents(titleInput);
 
-        modal.addComponents(repoActionRow, titleActionRow);
+        modal.addComponents(repoActionRow, titleActionRow, oldIssueNumberActionRow);
         await interaction.showModal(modal);
 
 
     } else if (!interaction.isModalSubmit()) { return; }
     else if (interaction.customId === 'newissue') {
-        const repoName = interaction.fields.getTextInputValue('repo');
+        repoName = interaction.fields.getTextInputValue('repo');
         const issueTitle = interaction.fields.getTextInputValue('title');
-        if (repoName && issueTitle) {
+        const oldIssueNumber = interaction.fields.getTextInputValue('oldIssueNumber');
+        if (repoName && issueTitle || reponame && oldIssueNumber) {
+
+            if (repoName === 'copnet' || repoName === 'medicnet' || repoName === 'carnet' || repoName === 'firenet') {
+                repoName = 'wgc/' + repoName + '.li';
+            }
+            else if (repoName === 'tablet') {
+                repoName = 'wgc/VPC-Connector-FiveM';
+            }
+            else {
+                repoName = null;
+            }
+
             try {
-                const response = await createIssue(repoName, issueTitle, interaction.user.tag);
-                const issueNumber = response.data.repository.full_name + "#" + response.data.number;
+                if (oldIssueNumber) {
+                const response = await createIssue(repoName, issueTitle, interaction.user.tag, oldIssueNumber);
+
+                const issueNumber = repoName + "#" + oldIssueNumber;
 
                 await interaction.client.application.fetch();
-                interaction.reply({ content: `Creating Issue...`, ephemeral: true }); // We need this so the form doesn't stay open
+                interaction.reply({ content: `Creating Follow Up...`, ephemeral: true }); // We need this so the form doesn't stay open
 
                 const embed = new EmbedBuilder()
                     .setTitle(`Internal Issue ${issueNumber}`)
-                    .setDescription(`The issue has been created for ${interaction.user.tag}.`)
+                    .setDescription(`Added Follow Up for: ${interaction.user.tag}.`)
                     .setColor('#008000')
-                    .setFooter({ text: `Created by ${interaction.client.application.owner.tag}` });
+                    .setFooter({ text: `Bot created by ${interaction.client.application.owner.tag}` });
                 await message.reply({ embeds: [embed] });
+
+                } else {
+                    const response = await createIssue(repoName, issueTitle, interaction.user.tag, oldIssueNumber);
+                    const issueNumber = response.data.repository.full_name + "#" + response.data.number;
+    
+                    await interaction.client.application.fetch();
+                    interaction.reply({ content: `Creating Issue...`, ephemeral: true }); // We need this so the form doesn't stay open
+    
+                    const embed = new EmbedBuilder()
+                        .setTitle(`Internal Issue ${issueNumber}`)
+                        .setDescription(`The issue has been created for ${interaction.user.tag}.`)
+                        .setColor('#008000')
+                        .setFooter({ text: `Bot created by ${interaction.client.application.owner.tag}` });
+                    await message.reply({ embeds: [embed] });
+    
+                }
             } catch (error) {
                 console.error(error);
                 await interaction.reply('An error occurred while creating the issue.');
             }
         } else {
-            await interaction.reply('You must provide a repo name and issue title to create an issue.');
+            await interaction.reply('You must provide repo name and title, or reponame and an existing issue number.');
         }
     }
 });
 
-async function createIssue(repoName, issueTitle, UserTag) {
+async function createIssue(repoName, issueTitle, UserTag, oldIssueNumber) {
 
+    if (oldIssueNumber) {
+        const url = `${process.env.GITEA_HOST}/api/v1/repos/${repoName}/issues/${oldIssueNumber}/comments`;
+        const messageContent = message.content;
 
-    if (repoName === 'copnet' || repoName === 'medicnet' || repoName === 'carnet' || repoName === 'firenet') {
-        repoName = 'wgc/' + repoName + '.li';
-    }
-    else if (repoName === 'tablet') {
-        repoName = 'wgc/VPC-Connector-FiveM';
-    }
-    else {
-        repoName = null;
-    }
-
-    const url = `${process.env.GITEA_HOST}/api/v1/repos/${repoName}/issues`;
-
-    const messageContent = message.content;
-
-    let body = `**Message:** ${messageContent}`;
-    if (imageUrls.length > 0) {
-        body += ` <br /><br />`;
-        for (const imageUrl of imageUrls) {
-            body += `<img src="${imageUrl}" alt="Image" />`;
+        let body = `**Follow up message:** ${messageContent}`;
+        if (imageUrls.length > 0) {
+            body += ` <br /><br />`;
+            for (const imageUrl of imageUrls) {
+                body += `<img src="${imageUrl}" alt="Image" />`;
+            }
         }
+        body += `<br /><br /> **Link:** ${message.url} \n **Message from:** ${author} \n**Created by:** ${UserTag}`;
+
+        const data = {
+            title: issueTitle,
+            body,
+        };
+
+        const config = {
+            headers: {
+                Authorization: `token ${process.env.GITEA_TOKEN}`,
+                'Content-Type': 'application/json',
+            },
+        };
+        return axios.post(url, data, config);
+
+    } else {
+        const url = `${process.env.GITEA_HOST}/api/v1/repos/${repoName}/issues`;
+        const messageContent = message.content;
+
+        let body = `**Message:** ${messageContent}`;
+        if (imageUrls.length > 0) {
+            body += ` <br /><br />`;
+            for (const imageUrl of imageUrls) {
+                body += `<img src="${imageUrl}" alt="Image" />`;
+            }
+        }
+        body += `<br /><br /> **Link:** ${message.url} \n **Message from:** ${author} \n**Created by:** ${UserTag}`;
+
+        const data = {
+            title: issueTitle,
+            body,
+        };
+
+        const config = {
+            headers: {
+                Authorization: `token ${process.env.GITEA_TOKEN}`,
+                'Content-Type': 'application/json',
+            },
+        };
+
+        return axios.post(url, data, config);
     }
-    body += `<br /><br /> **Link:** ${message.url} \n **Message from:** ${author} \n**Created by:** ${UserTag}`;
 
-    const data = {
-        title: issueTitle,
-        body,
-    };
-
-    const config = {
-        headers: {
-            Authorization: `token ${process.env.GITEA_TOKEN}`,
-            'Content-Type': 'application/json',
-        },
-    };
-
-    return axios.post(url, data, config);
 }
-
 
 client.login(process.env.BOT_TOKEN);
